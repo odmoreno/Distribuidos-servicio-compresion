2f8f46e24a7284be95d87c6cc07430f780dd3209
jebenite <evelynbenitez263@gmail.com>
2017-07-22 16:09:31 -0500

    Cola de borrar,lista

--- src/rpc_client.js
+++ src/rpc_client.js
@@ -5,41 +5,72 @@ var path = require('path');
 var fs = require('fs');
 var zip = new require('node-zip')();
 var args = process.argv.slice(2);
+idDelete="";
+idRead="";
 
+corr = generateUuid();
 if (args.length === 0) {
   console.log("Usage: rpc_client.js prioridad");
   process.exit(1);
 }
 queuePriority="Low"
-if(args[0]=="alta"){
-  queuePriority="High"
-}else{
-  queuePriority="Low"
+if(args[0]=="delete"){
+  queuePriority="High";
+  idDelete=args[1];
+  mensaje="[x] Enviando archivo a borrar con id: "+idDelete;
+}else if(args[0]=="create"){
+  queuePriority="Low";
+  mensaje='[x] Enviando archivo con id: '+corr;
+  //borrado="false";
+}else if (args[0]=="read"){
+  queuePriority="Normal";
+  idRead=args[1];
+  mensaje="[x] Enviando archivo a leer con id: "+idRead
+}
+else{
+  console.log("Se equivoco al escribir, intente de nuevo...");
+  return;
 }
 
 amqp.connect('amqp://hfmlwsqw:2zIpQS_S-FRv4A6Qgb1MJx2E0Zxz6PPW@orangutan.rmq.cloudamqp.com/hfmlwsqw', function(err, conn) {
   conn.createChannel(function(err, ch) {
     ch.assertQueue('', {exclusive: true}, function(err, q) {
-      var corr = generateUuid();
-      console.log(' [x] Enviando archivo');
-      var arch = ''+path.join(__dirname, 'file1.txt');
-      var bufferArch=fs.readFileSync(arch);
-      ch.consume(q.queue, function(msg) {
-        if (msg.properties.correlationId === corr) {
-          fs.writeFileSync('file_compressed.zip', msg.content, 'binary');
-          console.log(' [.] Archivo comprimido con éxito');
-          setTimeout(function() { conn.close(); process.exit(0) }, 500);
-        }
-      }, {noAck: true});
-      ch.sendToQueue(queuePriority,
-        new Buffer(bufferArch),
+      console.log(mensaje);
+      if(queuePriority=="Low"){
+        var arch = ''+path.join(__dirname, 'file1.txt');
+        var bufferArch=fs.readFileSync(arch);
+        ch.consume(q.queue, function(msg) {
+          if (msg.properties.correlationId === corr) {
+            if(msg.content.toString()==corr){
+              console.log("Archivo con id %s no comprimido ",corr);
+            }else{
+              fs.writeFileSync('file_compressed.zip', msg.content, 'binary');
+              console.log(' [.] Archivo con id %s comprimido con éxito',corr);
+            }
+            setTimeout(function() { conn.close(); process.exit(0) }, 500);
+          }
+        }, {noAck: true});
+        ch.sendToQueue(queuePriority,
+          new Buffer(bufferArch),
+          { correlationId: corr, replyTo: q.queue});
+      }
+      else if(queuePriority=="High"){
+        ch.consume(q.queue, function(msg) {
+          if (msg.properties.correlationId == corr) {
+            console.log(msg.content.toString());
+            setTimeout(function() { conn.close(); process.exit(0) }, 500);
+          }
+        }, {noAck: true});
+        ch.sendToQueue('High',
+        new Buffer(idDelete),
         { correlationId: corr, replyTo: q.queue });
+      }
     });
   });
 });
 
 function generateUuid() {
-  return Math.random().toString() +
-         Math.random().toString() +
-         Math.random().toString();
+  return Math.floor((Math.random() * 9) + 1).toString() +
+         Math.floor((Math.random() * 9) + 1).toString() +
+         Math.floor((Math.random() * 9) + 1).toString();
 }
--- src/rpc_server.js
+++ src/rpc_server.js
@@ -4,7 +4,7 @@ var amqp = require('amqplib/callback_api');
 var fs = require('fs');
 var zip = new require('node-zip')();
 var path = require('path');
-
+idFile="";
 amqp.connect('amqp://hfmlwsqw:2zIpQS_S-FRv4A6Qgb1MJx2E0Zxz6PPW@orangutan.rmq.cloudamqp.com/hfmlwsqw', function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'High';
@@ -12,16 +12,16 @@ amqp.connect('amqp://hfmlwsqw:2zIpQS_S-FRv4A6Qgb1MJx2E0Zxz6PPW@orangutan.rmq.clo
     ch.prefetch(1);
     console.log(' [x] Esperando requests de alta prioridad');
     ch.consume(q, function reply(msg) {
-      setTimeout(function(){
-        console.log(' [x] Archivo de alta prioridad obtenido');
-        zip.file("file.txt",msg.content);
-        var data = zip.generate({ base64:false, compression: 'DEFLATE' });
-        ch.sendToQueue(msg.properties.replyTo,
-          new Buffer(data),
-          {correlationId: msg.properties.correlationId});
-          ch.ack(msg);
-        }, 1);
-      });
+      console.log(' [x] Archivo a borrar obtenido');
+      idFile = msg.content.toString();
+      console.log(idFile);
+      idClientDelete=msg.properties.correlationId;
+      queueDelete=msg.properties.replyTo;
+      ch.sendToQueue(queueDelete,
+        new Buffer("Archivo borrado exitosamente"),
+        {correlationId: idClientDelete});
+        ch.ack(msg);
+    });
   });
   conn.createChannel(function(err, ch) {
     var q = 'Low';
@@ -30,14 +30,22 @@ amqp.connect('amqp://hfmlwsqw:2zIpQS_S-FRv4A6Qgb1MJx2E0Zxz6PPW@orangutan.rmq.clo
     console.log(' [x] Esperando requests de baja prioridad');
     ch.consume(q, function reply(msg) {
       setTimeout(function(){
-        console.log(' [x] Archivo de baja prioridad obtenido');
-        zip.file("file.txt",msg.content);
-        var data = zip.generate({ base64:false, compression: 'DEFLATE' });
-        ch.sendToQueue(msg.properties.replyTo,
-          new Buffer(data),
-          {correlationId: msg.properties.correlationId});
-        ch.ack(msg);
-      }, 6000);
+        if(msg.properties.correlationId==idFile){
+          ch.sendToQueue(msg.properties.replyTo,
+            new Buffer(idFile),
+            {correlationId: msg.properties.correlationId});
+            ch.ack(msg);
+        }
+        else{
+          console.log(' [x] Archivo de baja prioridad obtenido');
+          zip.file("file.txt",msg.content);
+          var data = zip.generate({ base64:false, compression: 'DEFLATE' });
+          ch.sendToQueue(msg.properties.replyTo,
+            new Buffer(data),
+            {correlationId: msg.properties.correlationId});
+          ch.ack(msg);
+        }
+      }, 10000);
     });
   });
 
